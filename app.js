a4aab119508fbc847eeef714a236ce1a9908e3ab
Akram Elgyar <125706128+AKRAMELGYAR@users.noreply.github.com>
2025-03-03 21:34:37 +0200

    v.0.1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
-
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+dotenv.config({path: './config/.env'})
 const mongoose = require('mongoose')
 const cors = require('cors')
 
